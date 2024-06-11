9a1f34afdd5cb99e3ae222f21c3167cf0bc6da00
usr <johnzuchler@gmail.com>
2024-06-11 17:20:48 +0200

    changes here

--- php/memes.php
+++ php/memes.php
@@ -1,15 +1,15 @@
 [
-        
-    <?php
+<?php
     $memes = array();
     $path = "../assets/memes";
     if ($handle = opendir($path)) {
         while (false !== ($entry = readdir($handle))) {
-            if ($entry != "." && $entry != ".." && is_file($dirPath . '/' . $entry)) {
-            echo '"' . $entry . '",';
+            if ($entry != "." && $entry != ".." && is_file($path . '/' . $entry)) {
+                echo '  "' . $path . "/" . $entry . '",
+';
             }
         }
         closedir($handle);
-    
-    ?>
+    }
+?>
 ]
