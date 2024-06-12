5bcd8b1ff8dbe8089622c2b13bf9534692370584
usr <johnzuchler@gmail.com>
2024-06-12 17:22:15 +0200

    I hate my life

--- php/memes.php
+++ php/memes.php
@@ -3,10 +3,15 @@
     $memes = array();
     $path = "../assets/memes";
     if ($handle = opendir($path)) {
+        $first = true;
         while (false !== ($entry = readdir($handle))) {
             if ($entry != "." && $entry != ".." && is_file($path . '/' . $entry)) {
-                echo '  "' . $path . "/" . $entry . '",
-';
+                if (!$first){
+                    echo ",";
+                }
+                echo ' 
+    "' . $path . "/" . $entry . '"';
+                $first = false;
             }
         }
         closedir($handle);
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./de/*.html", "./en/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
