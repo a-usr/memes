8c5b15d0619cde011bcfccd252c2d2d6397e4b96
usr <johnzuchler@gmail.com>
2024-06-18 14:34:43 +0200

    trim down on useless files

--- svg.py
+++
@@ -1,9 +0,0 @@
-import svgutils
-from svgutils.compose import Element
-from svgutils.transform import LineElement
-def main():
-    svg = svgutils.compose.SVG("./shared/background.svg")
-    last: Element = svg.find_id("last")
-    last = LineElement(last.root)
-if __name__ == "__main__":
-    main()
